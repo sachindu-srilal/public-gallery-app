e052ee551fcac78a7a402e505d5c8ef064c474fc
Sachindu Srilal <lal95.ac@gmail.com>
2023-06-24 22:28:21 +0530

    setup the app

---
+++ back-end/src/main/java/lk/ijse/dep10/gallery/WebRootConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep10.gallery;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebRootConfig implements WebMvcConfigurer {
+
+}
